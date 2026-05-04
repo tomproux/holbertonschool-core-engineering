49294c9425591066ac8c4bae690be608ff19214c
tomproux <tomproux53@gmail.com>
2026-05-04 15:32:41 +0200

    ok

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-for alphabt in alphabet:
-    if alphabt != 'e' and alphabt != 'q':
-        print(alphabt)
+for letter in alphabet:
+    if letter != 'e' and letter != 'q':
+        alphabt = letter
+
+print(alphabt)
