b9b1a7d0772de3c41a8a4d42c8e7d4a47b774e11
tomproux <tomproux53@gmail.com>
2026-05-04 14:24:32 +0200

    ok

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+number = __import__('random').randint(-10000, 10000)
+digit = number % 10
+if number > 0:
+    print("Last digit of", number, "is", digit, "and is greater than 5")
+
+if number == 0:
+    print("Last digit of", number, "is", digit, "and is 0")
+
+if number < 0:
+    print("Last digit of", number, "is", digit, "and is less than 6 and not 0")
