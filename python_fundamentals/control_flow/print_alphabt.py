04fb77b52dc180ded7315675757989e27226b2a2
tomproux <tomproux53@gmail.com>
2026-05-04 15:36:03 +0200

    ok

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabt = ""
 
 for letter in alphabet:
     if letter != 'e' and letter != 'q':
-        alphabt = letter
+        alphabt += letter
 
 print(alphabt)
