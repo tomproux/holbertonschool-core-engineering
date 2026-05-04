4cf51d7497383e7673022d9bffda507a9583766b
tomproux <tomproux53@gmail.com>
2026-05-04 15:21:34 +0200

    ok

---
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+for alphabt in alphabet:
+    if alphabt != "e" and alphabt != "q":
+        print(alphabt)
