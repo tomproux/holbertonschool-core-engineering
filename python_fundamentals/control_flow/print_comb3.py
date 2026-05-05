3611169b2f6fac29cc122fd17392f4f219900cb9
tomproux <tomproux53@gmail.com>
2026-05-05 13:05:22 +0200

    ok

---
+++ python_fundamentals/control_flow/print_comb3.py
@@ -0,0 +1,9 @@
+numbers = list(range(0, 90))
+for i in numbers:
+    s = str(i)
+    if i >= 0 and i < 99:
+        if i >= 0 and i < 10:
+            s = "0" + s
+        print("{}".format(s), end=", ")
+    else:
+        print("{}".format(s), end="\n")
