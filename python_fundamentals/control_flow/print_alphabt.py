051bdf4b0ee2f7bedd83b4827342d20ee3805bd9
tomproux <tomproux53@gmail.com>
2026-05-05 09:41:07 +0200

    ok

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-print("".join(c for c in "abcdefghijklmnopqrstuvwxyz" if c not in "qe"))
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabt = ""
+
+for letter in alphabet:
+    if letter != 'e' and letter != 'q':
+        alphabt += letter
+
+print(alphabt)
