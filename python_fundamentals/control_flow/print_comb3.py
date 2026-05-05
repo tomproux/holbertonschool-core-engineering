0310acfb55b1ef50a7ee1e60e5447c29a5dc139c
tomproux <tomproux53@gmail.com>
2026-05-05 13:50:36 +0200

    ok

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,9 +1,12 @@
 numbers = list(range(1, 90))
 for i in numbers:
+    digitUnit = i % 10
+    digitDeci = (i - digitUnit) / 10
     s = str(i)
-    if i >= 1 and i < 89:
-        if i >= 0 and i < 10:
-            s = "0" + s
-        print("{}".format(s), end=", ")
-    else:
-        print("{}".format(s), end="\n")
+    if digitUnit != digitDeci:
+        if i >= 1 and i < 89:
+            if i >= 0 and i < 10:
+                s = "0" + s
+            print("{}".format(s), end=", ")
+        else:
+            print("{}".format(s), end="\n")
