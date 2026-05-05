b07021a341020881e10f205ec6a6a21f352876e7
tomproux <tomproux53@gmail.com>
2026-05-05 10:30:54 +0200

    ok

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-'''alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 alphabt = ""
 
 for letter in alphabet:
     if letter != 'e' and letter != 'q':
         alphabt += letter
 
-print("{}".format(alphabt), end='\n')'''
-for lettre in range(ord('a'), ord('z') + 1):
-    if chr(lettre) != 'e' and chr(lettre) != 'q':
-        print("{}".format(chr(lettre)), end="")
-print()
+print("{}".format(alphabt), end='\n')
