f69f8e51410418ad531a6373efe7962ae95ebd80
tomproux <tomproux53@gmail.com>
2026-05-05 11:23:19 +0200

    ok

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 numbers = list(range(0, 100))
 for i in numbers:
-    print("{}".format(i), end="")
+    s = str(i)
+    if i >= 0 and i < 10:
+        s = "0" + s
+    print("{}".format(s), end="")
     if i < 99:
-        print(", ")
+        print(", ", end="")
