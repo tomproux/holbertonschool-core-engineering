851a1cc614d9d086b7552601630b034033ee7535
tomproux <tomproux53@gmail.com>
2026-05-07 15:22:30 +0200

    ok

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from calculator import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_sub))
+    print("{} * {} = {}".format(a, b, result_mul))
+    print("{} / {} = {}".format(a, b, result_div))
