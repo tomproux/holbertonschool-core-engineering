b5423ddb87736f2e9243a59cde371b85bedc895b
tomproux <tomproux53@gmail.com>
2026-05-07 11:27:24 +0200

    ok

--- python_fundamentals/functions_modules/islower.py
+++ python_fundamentals/functions_modules/islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def islower(c):
     alphabet = list(range(ord('a'), ord('z')))
+    c = ord(c)
     if c in alphabet:
-        c = chr(c)
         return True
     else:
         return False
