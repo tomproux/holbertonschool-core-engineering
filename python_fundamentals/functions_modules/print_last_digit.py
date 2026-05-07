37f6de8df03cf59e9935790456b2d435207eb5cd
tomproux <tomproux53@gmail.com>
2026-05-07 14:25:02 +0200

    ok

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
-    if number < 0:
-        digit = (-number % 10)
-    elif number >= 0 and number < 10:
-        print("{}".format(number))
-    else:
-        digit = number % 10
-    print("{}".format())
+    digit = abs(number) % 10
+    print("{}".format(digit), end="")
     return digit
