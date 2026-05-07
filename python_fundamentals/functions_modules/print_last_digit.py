58b1883bc1ce960f7d61225cb721a38e087b39e3
tomproux <tomproux53@gmail.com>
2026-05-07 14:21:10 +0200

    ok

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -2,9 +2,9 @@
 def print_last_digit(number):
     if number < 0:
         digit = (-number % 10)
-        '''elif number >= 0 and number < 11:
-        print("{}".format(number))'''
+    elif number >= 0 and number < 10:
+        print("{}".format(number))
     else:
         digit = number % 10
-    print("{}".format(digit))
+    print("{}".format())
     return digit
