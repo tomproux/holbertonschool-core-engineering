118157ce10d63e5e7240615078deff934427a119
tomproux <tomproux53@gmail.com>
2026-05-11 11:23:49 +0200

    ok

--- python_oop/classes_and_object_model/0-square.py
+++ python_oop/classes_and_object_model/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+"""
+Module that defines an empty Square class.
+"""
+
+
 class Square:
+    """
+    A class that defines a square.
+
+    This is an empty class for the first task.
+    """
     pass
