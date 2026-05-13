95b808b11404fefc91eac8109bcf768643a62d6a
tomproux <tomproux53@gmail.com>
2026-05-13 15:24:34 +0200

    ok

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
+"""Square module."""
+
+
 class Square:
-    """Square class with size validation."""
+    """Defines a square with size validation."""
 
     def __init__(self, size=0):
+        """Initialize a Square."""
         self.size = size
 
     @property
     def size(self):
-        """Get square size."""
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set square size with validation."""
+        """Set the size of the square with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
