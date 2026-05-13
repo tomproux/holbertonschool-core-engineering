da15a7837d7158e6c04d7847bfd931b6d9cd8b80
tomproux <tomproux53@gmail.com>
2026-05-13 15:08:11 +0200

    ok

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,8 +2,13 @@
 def pow(a, b):
     result = 1
 
-    while b > 0:
-        result *= a
-        b -= 1
+    if b > 0:
+        while b > 0:
+            result *= a
+            b -= 1
+    else:
+        while b < 0:
+            result /= a
+            b += 1
 
     return result
